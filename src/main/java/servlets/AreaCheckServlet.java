2df294c257ee18a7b2089fe9b10bae0a077b327a
svinoczar <alexsupper75@gmail.com>
2023-10-11 04:39:16 +0300

    reset up maven, javax and java servlets

--- src/main/java/servlets/AreaCheckServlet.java
+++ src/main/java/servlets/AreaCheckServlet.java
@@ -1,13 +1,67 @@
 package servlets;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/checkArea")
 public class AreaCheckServlet extends HttpServlet {
-    
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            float x = Float.parseFloat(request.getParameter("x"));
+            float y = Float.parseFloat(request.getParameter("y"));
+            float R = Float.parseFloat(request.getParameter("R"));
 
+            if (validate(x, y, R)) {
+                ResultRow resRow = new ResultRow(); // FIXME Переделать под http сессию
+                double start = System.nanoTime();
+                resRow.setHit(checkHit(x, y, R));
+                resRow.setX(x);
+                resRow.setY(y);
+                resRow.setR(R);
+                resRow.setCurrentTime(LocalDateTime.now());
+
+                double execTime = Math.round(((System.nanoTime() - start) * 0.00001) * 100.0) / 100.0;
+                resRow.setExecutionTime(execTime);
+                ResultTable resultTable = (ResultTable) request.getAttribute("resultTable");
+                resultTable.addNewElement(resRow);
+                getServletContext().setAttribute("resultRow", resRow);
+                getServletContext().setAttribute("resultTable", resultTable);
+            }
+        } catch (Exception e) {
+            getServletContext().setAttribute("error", e.getMessage());
+            request.getServletContext().getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+    }
+
+    public boolean validate(float x, float y, float R) {
+        if (3 < x || x < -5 || 5 < y || y < -3 || R == 1 || R == 1.5 || R == 2 || R == 2.5 || R == 3)
+            return false;
+        return true;
+    }
+
+    public boolean checkHit(float x, float y, float R) {
+        // Triangle 2nd quarter.
+        if (Math.abs(x) + Math.abs(y) <= R) { //FIXME rewrite if
+            return true;
+        }
+        // Circle 3rd quarter.
+        float dist = (float)Math.sqrt(x*x + y*y);
+        if (dist < R && x < 0 && y < 0) {
+            return true;
+        }
+        // Square 4th quarter.
+        if (x < R && 0 < x || y < 0 && -0.5*R < y) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/servlets/ControllerServlet.java
+++ src/main/java/servlets/ControllerServlet.java
@@ -1,9 +1,29 @@
 package servlets;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
 
-@WebServlet("/api")
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/controller")
 public class ControllerServlet extends HttpServlet {
+    private ResultTable resultTable;
 
+    public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            request.setAttribute("resultTable", resultTable);
+            getServletContext().getRequestDispatcher("/checkArea").forward(request, response);
+        } catch (Exception e) {
+            getServletContext().setAttribute("error", e.getMessage());
+            request.getServletContext().getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+    }
 }
